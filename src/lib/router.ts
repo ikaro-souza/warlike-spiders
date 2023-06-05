8639ef88b04e0a84036b454a5475f497b7eb45a3
ikaro-souza <0ikarosouza0@gmail.com>
2023-06-05 10:33:35 -0300

    uses window history api

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,19 +1,11 @@
 import { useRouter as useNextRouter, type NextRouter } from "next/router";
 
-function go(this: NextRouter, delta: number) {
-    for (let i = 0; i < Math.abs(delta); i++) {
-        if (delta < 0) {
-            this.back();
-        } else {
-            this.forward();
-        }
-    }
+function go(delta: number) {
+    window.history.go(delta);
 }
 
 type Router = NextRouter & { go: typeof go };
 export const useRouter = (): Router => {
     const router = useNextRouter();
-    go.bind(router);
-
     return { ...router, go };
 };
